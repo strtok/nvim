649052693b4640f9c48bbfb3faa86ef29e9645f7
Erik Bremen <strtok@gmail.com>
2024-03-23 08:01:02 -0700

    Add <leader>ce for rust error

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,4 @@ map("n", "<leader><Right>", "<C-w>l", { desc = "Go to right window", remap = tru
 map("n", "<leader><Left>", "<C-w>h", { desc = "Go to left window", remap = true })
 map("n", "<leader><Up>", "<C-w>k", { desc = "Go to up window", remap = true })
 map("n", "<leader><Down>", "<C-w>j", { desc = "Go to down window", remap = true })
+map("n", "<leader>ce", "<cmd>RustLsp explainError<cr>", { desc = "Explain Rust error", remap = true })
